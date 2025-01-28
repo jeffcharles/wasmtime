55255f778e83be4713bb47115f406c523facfa84
Jeff Charles <jeff.charles@shopify.com>
2025-01-28 19:46:30 +0000

    WIP

--- winch/codegen/src/visitor.rs
+++ winch/codegen/src/visitor.rs
@@ -385,6 +385,7 @@ macro_rules! def_unsupported {
     (emit F64x2Gt $($rest:tt)*) => {};
     (emit I8x16GeS $($rest:tt)*) => {};
     (emit I16x8GeS $($rest:tt)*) => {};
+    (emit I32x4GeS $($rest:tt)*) => {};
 
     (emit $unsupported:tt $($rest:tt)*) => {$($rest)*};
 }
@@ -3279,6 +3280,14 @@ where
         self.context
             .binop(self.masm, OperandSize::S16, |masm, dst, src, _size| {
                 masm.vector_ge(writable!(dst), dst, src, VectorCompareKind::I16x8S)?;
+                Ok(TypedReg::v128(dst))
+            })
+    }
+
+    fn visit_i32x4_ge_s(&mut self) -> Self::Output {
+        self.context
+            .binop(self.masm, OperandSize::S32, |masm, dst, src, _size| {
+                masm.vector_ge(writable!(dst), dst, src, VectorCompareKind::I32x4S)?;
                 Ok(TypedReg::v128(dst))
             })
     }
