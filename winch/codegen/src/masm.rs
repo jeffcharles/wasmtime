1c1a8b42888995aaa11c9b2a1706fe8fdcdfbb55
Jeff Charles <jeff.charles@shopify.com>
2025-01-31 20:01:27 +0000

    WIP

--- winch/codegen/src/masm.rs
+++ winch/codegen/src/masm.rs
@@ -548,7 +548,6 @@ impl V128NarrowKind {
     }
 }
 
-// FIXME see if there's a way to work this into `VectorExtendKind`
 /// Kinds of vector extending operations supported by WebAssembly.
 pub(crate) enum V128ExtendKind {
     /// Low half of i8x16 sign extended.
